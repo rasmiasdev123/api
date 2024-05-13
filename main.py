3aa2a72edf827051e713f236d0f089311f847b0b
Rasmi Ranjan <updatelook15@email.com>
2024-05-13 14:36:00 +0530

    entry 1

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    result = fetch_fno()
-    print(result)
+    # result = fetch_fno()
+    # print(result)
     return {"result": "this is for testing"}
